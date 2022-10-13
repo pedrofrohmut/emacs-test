1be77cfd651263c5073bfc8e7e834006c30c7f5a
Pedro Frohmut <pedrofrohmut@gmail.com>
2022-10-12 23:38:48 -0300

    Transparency for emacs in terminal mode

--- early-init.el
+++ early-init.el
@@ -15,3 +15,13 @@
 (set-fringe-mode 12) ;; Adds side padding to frames
 
 (setq inhibit-startup-message t)
+
+;; Tranparency for Terminal #####################################################
+(defun set-background-for-terminal (&optional frame)
+  (or frame (setq frame (selected-frame)))
+  "unsets the background color in terminal mode"
+  (unless (display-graphic-p frame)
+    (set-face-background 'default "unspecified-bg" frame)))
+
+(add-hook 'after-make-frame-functions 'set-background-for-terminal)
+(add-hook 'window-setup-hook          'set-background-for-terminal)
